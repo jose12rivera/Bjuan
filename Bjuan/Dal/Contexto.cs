a025a5cfc9688f5f2fbf28784fe861b0cacb4bc2
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-16 09:03:08 -0400

    Add project files.

---
+++ Bjuan/Dal/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Bjuan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bjuan.Dal
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto>options)
+        :base(options){ }
+
+        public DbSet<Articulos> Articulos { get; set; }
+    }
+}
---
+++ Bjuan/Models/Articulos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bjuan.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        [Required(ErrorMessage = "intentar")]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
